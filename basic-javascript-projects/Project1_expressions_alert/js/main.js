547b732b15c363b60986e497907cf8250c17d8d5
augustgoodlund <164281323+augustgoodlund@users.noreply.github.com>
2024-04-05 10:14:34 -0700

    added code

--- basic-javascript-projects/Project1_expressions_alert/js/main.js
+++ basic-javascript-projects/Project1_expressions_alert/js/main.js
@@ -1,1 +1,5 @@
-alert("Hello, World!");
+document.write('Uh, you mean nobody\'s asked you? Mayor Goldie Wilson, I like the sound of that. Alright kid, you stick to your father like glue and make sure that he takes her to the dance. Look me up when you get there, guess I\'ll be about 47. Say that again.' + 'Well gee, I don\'t know. What? You do? Alright, okay. Alright, there she is, George. Just go in there and invite her.');
+var A="Concatenated" + "String";
+window.alert(A);
+var Family="The Arezzinis", Dad="Jeremiah", Mom="Hermione", Daughter="Penny", Son="Zorro";
+document.write(Dad);
