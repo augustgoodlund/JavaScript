41a94328d6c9b9aa7c6ce922748ef6b61cd840c3
augustgoodlund <164281323+augustgoodlund@users.noreply.github.com>
2024-04-09 11:25:00 -0700

    Added second project

---
+++ basic-javascript-projects/Project2_functions/js/main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var str="Hiya buddy";
+    document.getElementById("demo").innerHTML=str;
+  }
+  function my2ndFunction() {
+    var sentence="I am learning";
+    sentence += " a lot from this class!";
+    document.getElementById("Concatenate").innerHTML=sentence;
+  }
