7179498d23d5ae7a1309097b41e45f294f0d4e51
augustgoodlund <164281323+augustgoodlund@users.noreply.github.com>
2024-04-09 16:41:24 -0700

    Added dictionaries assignment

--- basic-javascript-projects/Project4_dictionaries/js/main.js
+++ basic-javascript-projects/Project4_dictionaries/js/main.js
@@ -1,7 +1,11 @@
-const person = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 50,
-    status: "marketing contact",
-    document.getElementById("Dictionary").innerHTML = person,
-};
+function myPlants() { //creates and names the dictionary
+    var Plant = { //creates and names an object in the dictionary
+        Species: "Pearls and Jade Pothos", //adds "Species" key
+        Color: "Variegated", //adds "Color" key
+        Size: "Large", //adds "Size" key
+        Watered: "Yes", //adds "Watered" key
+        Repotted: "Yes" //adds "Repotted" key
+    };
+    delete Plant.Repotted; //deletes "Repotted" key
+    document.getElementById("watered").innerHTML=Plant.Watered; //creates an ID for the key "watered" and assigns it a keyword for the HTML file
+  }
