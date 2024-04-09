ee9d1f4ef8eb228bd6b26f556c28ca337d06a44a
augustgoodlund <164281323+augustgoodlund@users.noreply.github.com>
2024-04-09 10:34:31 -0700

    added comments

--- basic-javascript-projects/Project1_expressions_alert/js/main.js
+++ basic-javascript-projects/Project1_expressions_alert/js/main.js
@@ -1,11 +1,11 @@
-document.write('Uh, you mean nobody\'s asked you? Mayor Goldie Wilson, I like the sound of that. Alright kid, you stick to your father like glue and make sure that he takes her to the dance. Look me up when you get there, guess I\'ll be about 47. Say that again.' + 'Well gee, I don\'t know. What? You do? Alright, okay. Alright, there she is, George. Just go in there and invite her.');
-var A="Concatenated" + "String";
-window.alert(A);
-var Family="The Arrezinis", Dad="Jeremiah", Mom="Hermione", Daughter="Penny", Son="Zorro";
-document.write(Dad);
-var x=10;
-document.write(x+3);
-function My_First_Function() { //Defines a function and names it, gives it a string value
-    var str="This is the button text!";
-    document.getElementById("Button_Text").innerHTML=str;
+var Sent1="This is the beginning of the string"; //assigns a string as a variable
+var Sent2=" and this is the end of the string"; //assigns a second string as a second variable
+var A=Sent1 + Sent2; //concats the two variables
+window.alert(A); //displays the concatenated variable as an alert
+var Family="The Arrezinis", Dad="Jeremiah", Mom="Hermione", Daughter="Penny", Son="Zorro"; //assigns several variables in one line
+var x=10; // assigns variable as the number 10
+document.write(x+3); //displays the result of a math problem utilizing the previous variable as text
+function My_First_Function() { //Defines a function and names it
+    var str="This is the button text!"; //assigns button a string value
+    document.getElementById("Button_Text").innerHTML=str; // uses button_text ID to put the button in the HTML file
 }
