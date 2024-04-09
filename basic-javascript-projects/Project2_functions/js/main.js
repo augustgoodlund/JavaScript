0b45f108a1a5de6b99fffe4de99749e15ef6a303
augustgoodlund <164281323+augustgoodlund@users.noreply.github.com>
2024-04-09 11:28:52 -0700

    added comments

--- basic-javascript-projects/Project2_functions/js/main.js
+++ basic-javascript-projects/Project2_functions/js/main.js
@@ -1,9 +1,9 @@
-function myFunction() {
-    var str="Hiya buddy";
-    document.getElementById("demo").innerHTML=str;
+function myFunction() { //creates and names a function
+    var str="Hiya buddy"; //assigns a string to the variable used in the function
+    document.getElementById("demo").innerHTML=str; //uses getElementById to link function to HTML document using the id "demo"
   }
-  function my2ndFunction() {
-    var sentence="I am learning";
-    sentence += " a lot from this class!";
-    document.getElementById("Concatenate").innerHTML=sentence;
+  function my2ndFunction() { //creates and names another function
+    var sentence="I am learning"; //assigns a string to a variable
+    sentence += " a lot from this class!"; //concatenates previous variable with a second string
+    document.getElementById("Concatenate").innerHTML=sentence; //links the concatenated strings as a new value to the HTML document
   }
