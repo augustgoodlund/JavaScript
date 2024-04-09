c622e0cbeda6ad8acd747856648559eb8b6aa2ea
augustgoodlund <164281323+augustgoodlund@users.noreply.github.com>
2024-04-09 10:25:04 -0700

    Updated code

--- basic-javascript-projects/Project1_expressions_alert/js/main.js
+++ basic-javascript-projects/Project1_expressions_alert/js/main.js
@@ -1,5 +1,11 @@
 document.write('Uh, you mean nobody\'s asked you? Mayor Goldie Wilson, I like the sound of that. Alright kid, you stick to your father like glue and make sure that he takes her to the dance. Look me up when you get there, guess I\'ll be about 47. Say that again.' + 'Well gee, I don\'t know. What? You do? Alright, okay. Alright, there she is, George. Just go in there and invite her.');
 var A="Concatenated" + "String";
 window.alert(A);
-var Family="The Arezzinis", Dad="Jeremiah", Mom="Hermione", Daughter="Penny", Son="Zorro";
+var Family="The Arrezinis", Dad="Jeremiah", Mom="Hermione", Daughter="Penny", Son="Zorro";
 document.write(Dad);
+var x=10;
+document.write(x+3);
+function My_First_Function() { //Defines a function and names it, gives it a string value
+    var str="This is the button text!";
+    document.getElementById("Button_Text").innerHTML=str;
+}
