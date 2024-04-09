28d5986bf066cc493308bc0021235a0f704327b4
augustgoodlund <164281323+augustgoodlund@users.noreply.github.com>
2024-04-09 16:14:51 -0700

    Updated code

--- basic-javascript-projects/Project3_math_operators/js/main.js
+++ basic-javascript-projects/Project3_math_operators/js/main.js
@@ -1,39 +1,27 @@
-function addFunction(p1, p2) {
-    return p1 + p2;
-  }
-  function subtractFunction(p1, p2) {
-    return p1 - p2;
-  }
-  function multiplyFunction(p1, p2) {
-    return p1 * p2;
-  }
-  function divideFunction(p1, p2) {
-    return p1 / p2;
-  }
-  
-let result = addFunction(4, 3);
-document.getElementById("add").innerHTML = result;
 
-let result2 = subtractFunction(4, 3);
-document.getElementById("subtract").innerHTML = result2;
+function addFunction() {
+document.getElementById("add").innerHTML = 4+3;
+}
 
-let result3 = multiplyFunction(4, 3);
-document.getElementById("multiply").innerHTML = result3;
+function subtractFunction() {
+  document.getElementById("subtract").innerHTML=4-3;
+}
 
-let result4 = divideFunction(4, 3);
-document.getElementById("divide").innerHTML = result4;
+function multiplyFunction() {
+  document.getElementById("multiply").innerHTML=4*3;
+}
 
 function more_Math() { //multi-step math problem
     var simple_Math = (1+2)*10/2-5;
-    document.getElementById("Math").innerHTML="1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
+    document.getElementById("Math").innerHTML=simple_Math;
 }
 function modulus_Operator() { //divides and produces the remainder
     var simple_Math2 = 83%5;
-    document.getElementById("modOper").innerHTML="When you divide 83 by 5 you have a remainder of: " + simple_Math2;
+    document.getElementById("modOper").innerHTML=simple_Math2;
 }
 function negation_Operator() { //returns the opposite of the variable
     var x=10;
-    document.getElementById("Math").innerHTML=-x;
+    document.getElementById("negOper").innerHTML=-x;
 }
 var X=5; //increases by 1 increment
 X++;
@@ -41,6 +29,8 @@ document.write(X);
 var Y=5.25; //reduces by discrete amounts
 Y--;
 document.write(Y);
-window.alert(Math.random()); //returns a random number between 0 and 1
-window.alert(Math.random() * 100); //random number between 0 and 100
-document.getElementById("power").innerHTML = Math.pow(8,2); //utilizes math object to return a value of x to the power of y
+document.write(Math.random()); //returns a random number between 0 and 1
+document.write(Math.random() * 100); //random number between 0 and 100
+function powOper() {
+  document.getElementById("power").innerHTML = Math.pow(8,2); //utilizes math object to return a value of x to the power of y
+  }
---
+++ basic-javascript-projects/Project4_dictionaries/js/main.js
@@ -0,0 +1,7 @@
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    status: "marketing contact",
+    document.getElementById("Dictionary").innerHTML = person,
+};
