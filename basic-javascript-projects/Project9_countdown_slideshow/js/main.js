8e0064c3391ca6a33028dfcd97530b9829e453ec
augustgoodlund <164281323+augustgoodlund@users.noreply.github.com>
2024-04-10 17:32:25 -0700

    Added countdown syntax

--- basic-javascript-projects/Project9_countdown_slideshow/js/main.js
+++ basic-javascript-projects/Project9_countdown_slideshow/js/main.js
@@ -1,18 +1,13 @@
-//function countdown() { //creates and names the function
-//  var seconds=document.getElementById("seconds").value; //creates a new variable "seconds" and assigns it an ID for the HTML document
-//
-//function tick() { //creates and names the nested inner function
-//  seconds=seconds-1; //creates variable with an interval that decreases by 1
-//timer.innerHTML=seconds; //sets variable "seconds" as the timer
-//    var time=setTimeout(tick, 1000); //the program pauses for 1000ms, 1 second
-//    if (seconds==-1) { //IF statement: if the seconds can be comparable to decrease of 1
-//        alert("Time's up!");
-//        clearTimeout(time);
-//        timer.innerHTML="";
-//    }
-//}
-//tick();
-//}
+function countdown() { //creates and names the function
+  var seconds=document.getElementById("seconds").value; //creates a new variable "seconds" and assigns it an ID for the HTML document
+
+function tick() { //creates and names the nested inner function
+  seconds=seconds-1; //creates variable with an interval that decreases by 1
+timer.innerHTML=seconds; //sets variable "seconds" as the timer
+    var time=setTimeout(showslides, 10000); //sets the timeout at 10 seconds
+}
+tick();
+}
 let slideIndex=1;
 showSlides(slideIndex);
 //NEXT/PREV CONTROLS
