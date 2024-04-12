199f4652ae01eaad6212d4d65f0c3b903fdcfcf2
augustgoodlund <164281323+augustgoodlund@users.noreply.github.com>
2024-04-12 16:45:46 -0700

    added tictactoe game

--- basic-javascript-projects/test/js/main.js
+++ basic-javascript-projects/test/js/main.js
@@ -9,3 +9,10 @@ for (let i = 0; i < 10; i++) { //creates a for loop; the variable i is set to 0 
   text2 += "The number is " + i + "<br>"; //print the variable "text" plus the number i and then a line break
 }
 document.getElementById("continuestatement").innerHTML = text2;
+var ages=[2, 10, 18, 20, 13, 15, 30];
+//uses the arrow function to pass in a parameter of age
+checkAge = (age) => age >= 18;
+function ageFunction() {
+  //use the .some method to iterate through the array of ages and display the result. Once the function gets to 18 in the array it won’t check the rest of the variables in the ages array, as 18 fulfills the function requirement.
+  document.getElementById('displayAge').innerHTML=ages.some(checkAge);
+}
